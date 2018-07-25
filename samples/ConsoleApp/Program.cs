91f0a66dd38f8197c97af8f8bc4fabb08db0ea04
Tom Jerzak <tomasz.jerzak@gmail.com>
2018-07-25 12:46:01 +0200

    Verify 'dotnet seeder' in command line.

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// To run: 'dotnet seeder' in command line.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Seeder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Seeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seeder!");
+            if (args != null && args.Length > 0)
+            {
+                foreach (var arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
